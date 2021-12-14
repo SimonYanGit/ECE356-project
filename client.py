ab007e33c3d611e5a6f12d8d28f093fb35ed6b42
Pratham Thukral <27prathamthukral@gmail.com>
2021-12-14 12:40:04 -0500

    changed pw

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import mysql.connector
 db = mysql.connector.connect(
     host = "marmoset04.shoshin.uwaterloo.ca",
     user = "zj2yan",
-    passwd = "dbiojGHP2WU8zh@H63%8",
+    passwd = "Simon_123",
     database = "NHL_356"
 )
 cur = db.cursor()
